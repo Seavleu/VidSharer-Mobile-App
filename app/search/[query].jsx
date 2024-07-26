7ee692abc2569bb2c646d14c8369914f94e24002
Jenny Heang <86590058+Seavleu@users.noreply.github.com>
2024-07-26 09:17:38 +0900

    feat (home): implemented search query

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -3,15 +3,56 @@ import { useLocalSearchParams } from "expo-router";
 import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import useAppWrite from "../../lib/useAppWrite";
+import { searchPosts } from "../../lib/appwrite";
+import { SearchInput, EmptyState, VideoCard } from "../../components";
 
 const Search = () => {
-  const {query} = useLocalSearchParams();
-  
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppWrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <Text className="text-white text-center text-3xl">{query}</Text>
-    </SafeAreaView>
-  )
-}
+    <FlatList
+      data={posts}
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => (
+        <VideoCard
+          title={item.title}
+          thumbnail={item.thumbnail}
+          video={item.video}
+          creator={item.creator.username}
+          avatar={item.creator.avatar}
+        />
+      )}
+      ListHeaderComponent={() => (
+        <>
+          <View className="flex my-6 px-4">
+            <Text className="font-pmedium text-gray-100 text-sm">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white mt-1">
+              {query}
+            </Text>
 
-export default Search
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query} refetch={refetch} />
+            </View>
+          </View>
+        </>
+      )}
+      ListEmptyComponent={() => (
+        <EmptyState
+          title="No Videos Found"
+          subtitle="No videos found for this search query"
+        />
+      )}
+    />
+  </SafeAreaView>
+);
+};
+export default Search;
